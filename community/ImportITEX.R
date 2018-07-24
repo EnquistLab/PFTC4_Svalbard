9d1cf535d06237ede0b8bd16f9a113d7f12c8622
audhalbritter <audhalbritter.com>
2018-07-24 11:44:03 +0200

    itex codes

--- community/ImportITEX.R
+++ community/ImportITEX.R
@@ -24,7 +24,7 @@ itex %>%
   summarise(min = min(sum), max = max(sum))
 
 # Calculate species that sum up to 95% cover
-itex %>% 
+Cover95 <- itex %>% 
   filter(!GFNARROWarft %in% c("LICHEN", "MOSS", "LIVERWORT")) %>% 
   mutate(SP = paste(GENUS, SPECIES, sep = "_")) %>% 
   group_by(PLOT) %>% 
@@ -32,5 +32,21 @@ itex %>%
   mutate(cumprop = cumsum(HITS)/sum(HITS)) %>% 
   filter(cumprop <= 0.95) %>% 
   ungroup() %>% 
-  count(SP) %>% print(n = Inf)
+  count(SP)
 
+
+
+
+field <- itex %>% 
+  filter(!GFNARROWarft %in% c("LICHEN", "MOSS", "LIVERWORT")) %>% 
+  select(TREATMENT, PLOT, GENUS, SPECIES, HITS) %>% 
+  arrange(TREATMENT, PLOT, GENUS)
+
+itex.codes <- field %>% distinct(TREATMENT, PLOT) %>% 
+  mutate(Habitat = substr(PLOT, 1, 3)) %>% 
+  mutate(Plot = paste(PLOT, TREATMENT, sep = "-")) %>% 
+  mutate(Plot = substr(Plot, 6, nchar(Plot))) %>% 
+  select(-TREATMENT, -PLOT) %>% 
+  arrange(Habitat)
+  
+#write_csv(field, path = "FieldSheetITEX.csv")
