29f4a89a90daefd43967cf29a090fc81c46c316b
audhalbritter <audhalbritter.com>
2018-09-03 13:22:21 +0200

    remove tasks for PFTC course

--- traits/Rdatagathering/CalculateLeafArea.R
+++ traits/Rdatagathering/CalculateLeafArea.R
@@ -2,12 +2,12 @@
 #### CALCULATE LEAF AREA ####
 
 # load libraries
-devtools::install_github("richardjtelford/LeafArea")
+#devtools::install_github("richardjtelford/LeafArea")
 library("LeafArea")
 library("tidyverse")
 
 
-#### TASK Check LeafIDs
+#### Check LeafIDs
 # Load trait IDs
 load("traits/Rdatagathering/envelope_codes.Rdata", verbose = TRUE)
 
@@ -45,43 +45,14 @@ loop.files <-  function(files){
 
 
 
-#### TASK: Find the scans from these leaves. Describe potential problems for these leaves
-AEC8296
-AJI6590
-ALW3077
-AUJ7139
-AUL1863
-AVE0287
-BWF1270
-CAF5903
-BJC4868
-AFO1112
-AWU0779
-BWZ2813
-
-
-#### TASK: Are there consistent problems with some species that should be considered when calculating the leaf area.
-
-
-
-#### TASK: Calculate the leaf area using run.ij and check if there are problem.
+#### Calculate the leaf area using run.ij and check if there are problem.
 
 # test run.ij
 run.ij(set.directory = "~/Desktop/TestLeaf", distance.pixel = 237, known.distance = 2, log = TRUE, low.size = 0.005, trim.pixel = 55, trim.pixel2 = 150, save.image = TRUE)
 
 
-# Load LeafArea.raw.Rdata
-load(file = "traits/data/LeafArea.raw.Rdata", verbose = TRUE)
-
-
-# TASK: Calculate leaf area per LeafID
-
-
-# TASK: Check duplicate leaves, e.g. exact same leaf area
-
-
 #### Run run.ij to get areas
-list.of.files <- dir(path = paste0("/Volumes/Ohne Titel/Leaf Scans/2018-07-24/"), pattern = "jpeg|jpg", recursive = TRUE, full.names = TRUE)
+list.of.files <- dir(path = paste0("/Volumes/Ohne Titel/Leaf Scans"), pattern = "jpeg|jpg", recursive = TRUE, full.names = TRUE)
 
 new.folder <- "/Volumes/Ohne Titel/Temp/"
 output.folder <- "/Volumes/Ohne Titel/Leaf_Output/"
