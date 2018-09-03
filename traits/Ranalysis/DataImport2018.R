72e9a99990268531a07125c74e8d08d0f7343d10
audhalbritter <audhalbritter.com>
2018-09-03 17:02:37 +0200

    remove joining traits and community data

--- traits/Ranalysis/DataImport2018.R
+++ traits/Ranalysis/DataImport2018.R
@@ -266,38 +266,6 @@ communityRaw1 <- communityRaw %>%
   mutate(Elevation = as.character(Elevation))
     
 
-# Check
+# Check taxon not in traits or comm.
 setdiff(communityRaw1$Taxon, traits2018$Taxon)
 setdiff(traits2018$Taxon, communityRaw1$Taxon)
-
-### need to check how many do not join traits and community.
-### and how to join community and itex etc.
-
-
-traits %>% 
-  filter(!Project %in% c("P", "M", "Sean"), Site != "X") %>% 
-  anti_join(community.raw1, by = c("Site", "Elevation", "Plot", "Taxon")) %>% 
-  filter(!is.na(Elevation)) %>% 
-  filter(!is.na(Plot)) %>% 
-  select(Site, Elevation, Plot, Taxon) %>% pn
-
-### Join trait and community data
-# used left_join because we are interested in the traits for now, but species with low cover and no trait data get lost!
-TraitsCommGradients <- traits2018 %>% 
-  filter(!Project %in% c("P", "M", "Sean"), Site != "X") %>% 
-  left_join(community.raw1, by = c("Site", "Elevation", "Plot", "Taxon"))
-save(TraitsCommGradients, file = "traits/data/TraitsCommGradients.Rdata")
-
-TraitsAllProjects <- traits2018
-save(TraitsAllProjects, file = "traits/data/TraitsAllProjects.Rdata")
-
-
-### ITEX
-TraitsCommITEX <- traits2018 %>% 
-  filter(Site == "X") %>% 
-  left_join(itex1, by = c("Elevation", "Plot", "Taxon"))
-save(TraitsCommITEX, file = "traits/data/TraitsCommITEX.Rdata")
-
-
-
-setdiff(traits1$Taxon, itex1$Taxon)
