8cd5d85adafb7ac247a9d6d5bf9827360d850a21
bmaitner <bmaitner@gmail.com>
2018-09-24 13:14:55 -0700

    added code to calc the community mean, other moments and cis from the set of distributions.

--- trait_distributions/2_analyze_trait_distributions.R
+++ trait_distributions/2_analyze_trait_distributions.R
@@ -1,1 +1,11 @@
 #Summarize and analyze trait distributions
+#Brian Maitner
+
+source("trait_distributions/r_functions/summarize_moments.R")
+
+
+#Extract the trait moments from the distributions
+itex_moments<-extract_moments_itex(file_directory = "trait_distributions/itex_distributions/")
+
+saveRDS(object = itex_moments[c(1:4,6)],file = "trait_distributions/community_weighted_means.RDS")
+
