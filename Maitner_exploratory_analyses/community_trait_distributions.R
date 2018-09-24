4a49ace8b859fa5696545e30cc2d250032b30723
bmaitner <bmaitner@gmail.com>
2018-09-24 09:15:19 -0700

    added some code apparently. *shrug*

---
+++ Maitner_exploratory_analyses/community_trait_distributions.R
@@ -0,0 +1,11 @@
+#Calculating trait distributions from community and trait data collected by PFTC4Svalbard
+library(xlsx)
+library(funrar)
+library(BIEN)
+library(ggplot2)
+
+
+source("C:/Users/Brian/Desktop/current_projects/PFTC4_Svalbard/Maitner_exploratory_analyses/r_fxs/abundance_trait_distributions.R")
+source("C:/Users/Brian/Desktop/current_projects/PFTC4_Svalbard/Maitner_exploratory_analyses/r_fxs/trait_distribution_fx.R")
+source("C:/Users/Brian/Desktop/current_projects/PFTC4_Svalbard/Maitner_exploratory_analyses/r_fxs/extract_moments.R")
+
