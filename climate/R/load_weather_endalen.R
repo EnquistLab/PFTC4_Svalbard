fdbeb276c09d3932d8912096ed3c23a6ca267e10
audhalbritter <me@audhalbritter.com>
2020-04-16 18:20:11 +0200

    changing mutate_at to across rename

--- climate/R/load_weather_endalen.R
+++ climate/R/load_weather_endalen.R
@@ -37,31 +37,20 @@ WeatherStation <- list.files(path = "climate/data/DATA_ITEX_2015_2018/Hobo_weath
   map(read_xl_csv) %>% 
   map(select, -`#`) %>% 
   map(mutate, `PAR, uE (LGR S/N: 2271742, SEN S/N: 2008892, LBL: Weatherstn_SSE)` = gsub(",", "", `PAR, uE (LGR S/N: 2271742, SEN S/N: 2008892, LBL: Weatherstn_SSE)`)) %>% 
-  map_df(mutate_at, .vars = vars(-matches("Date|Time")), .fun = as.numeric, .id = "file") %>% 
+  map_df(~ mutate(.x, across(-matches("Date|Time"), as.numeric)), .id = "file") %>% 
   mutate(
-    date_time = if_else(!is.na(Date), paste(Date,  `Time, GMT+00:00`), NA_character_),
-    date_time = coalesce(date_time, `Date Time, GMT+00:00`), 
-    DateTime = lubridate::mdy_hms(date_time)) %>% 
+    DateTime = if_else(!is.na(Date), paste(Date,  `Time, GMT+00:00`), NA_character_),
+    DateTime = coalesce(DateTime, `Date Time, GMT+00:00`), 
+    DateTime = lubridate::mdy_hms(DateTime)) %>% 
   select(-Date, -`Date Time, GMT+00:00`, -`Time, GMT+00:00`) %>% 
   rename(
     PAR = `PAR, uE (LGR S/N: 2271742, SEN S/N: 2008892, LBL: Weatherstn_SSE)`,
     WaterContent = `Water Content, m³/m³ (LGR S/N: 2271742, SEN S/N: 2268271, LBL: Weatherstn_SSE)`,
     Temperature = `Temp, °C (LGR S/N: 2271742, SEN S/N: 10655574, LBL: Weatherstn_SSE)`,
     RelHumidity = `RH, % (LGR S/N: 2271742, SEN S/N: 10655574, LBL: Weatherstn_SSE)`,
-    SolarRadiation = `Solar Radiation, W/m² (LGR S/N: 2271742, SEN S/N: 10663215, LBL: Weatherstn_SSE)`
-    #wat_cont2 = `Water Content, mÂ³/mÂ³ (LGR S/N: 2271742, SEN S/N: 2268271, LBL: Weatherstn_SSE)`, 
-    #temp2 = `Temp, Â°C (LGR S/N: 2271742, SEN S/N: 10655574, LBL: Weatherstn_SSE)`,
-    #sol_rad2 = `Solar Radiation, W/mÂ² (LGR S/N: 2271742, SEN S/N: 10663215, LBL: Weatherstn_SSE)`
-    ) %>%  
+    SolarRadiation = `Solar Radiation, W/m² (LGR S/N: 2271742, SEN S/N: 10663215, LBL: Weatherstn_SSE)`) %>%  
   # combine species case
   bind_rows(specialFile) %>% 
-  # this does not seem to be needed
-  # mutate(
-  #   wat_cont = coalesce(`wat_cont1`, `wat_cont2`), 
-  #   temp = coalesce(temp1, temp2),
-  #   sol_rad = coalesce(sol_rad1, sol_rad2),
-  #   wat_cont = coalesce(wat_cont1, wat_cont2)) %>% 
-  #select(-matches('\\d')) %>% 
   mutate(
     WaterContent = if_else(WaterContent < -100, NA_real_, WaterContent)
   ) %>% 
@@ -76,17 +65,17 @@ WeatherStation <- list.files(path = "climate/data/DATA_ITEX_2015_2018/Hobo_weath
 
 
 ### Plotting and checking data
-# ggplot(data = hobo)+
+# ggplot(data = WeatherStation)+
 #   geom_point(mapping = aes(x = DateTime, y = PAR, color = file), show.legend = FALSE)
 # 
-# ggplot(data = hobo)+
+# ggplot(data = WeatherStation)+
 #   geom_point(mapping = aes(x = DateTime, y = WaterContent, color = file), show.legend = FALSE)
 # 
-# ggplot(data = hobo)+
+# ggplot(data = WeatherStation)+
 #   geom_point(mapping = aes(x = DateTime, y = RelHumidity, color = file), show.legend = FALSE)
 # 
-# ggplot(data = hobo)+
+# ggplot(data = WeatherStation)+
 #   geom_point(mapping = aes(x = DateTime, y = Temperature, color = file), show.legend = FALSE)
 # 
-# ggplot(data = hobo)+
+# ggplot(data = WeatherStation)+
 #   geom_point(mapping = aes(x = DateTime, y = SolarRadiation, color = file), show.legend = FALSE)
