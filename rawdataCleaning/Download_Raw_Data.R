cb6108f3aac603ce6f2334ea9ad81da5e7af040b
audhalbritter <me@audhalbritter.com>
2020-04-21 09:04:37 +0200

    download raw data

--- rawdataCleaning/Download_Raw_Data.R
+++ rawdataCleaning/Download_Raw_Data.R
@@ -1,10 +1,11 @@
-#### DOWNLOAS RAW DATA FROM OSF ####
+#### DOWNLOAD RAW DATA FROM OSF ####
 
 #install.packages("remotes")
 #remotes::install_github("Plant-Functional-Trait-Course/PFTCFunctions")
 library("PFTCFunctions")
 
-# Download raw climate data from OSF
+#### CLIMATE ####
+# Raw climate data
 get_file(node = "smbqh",
          file = "Climate_Data_ITEX_2015_2018.zip",
          path = "climate/raw_data",
@@ -16,8 +17,39 @@ outDir <- "climate/raw_data/"
 unzip(zipFile, exdir = outDir)
 
 
-# Download raw climate data from OSF
+#### META DATA ####
+# Meta data
+get_file(node = "smbqh",
+         file = "PFTC4_Svalbard_Coordinates.xlsx",
+         path = "traits/cleaned_Data",
+         remote_path = "MetaData")
+
+
+#### TRAITS ####
+# Raw leaf trait data
 get_file(node = "smbqh",
          file = "LeafTrait_Svalbard_with_DM.csv",
          path = "traits/data",
          remote_path = "RawData/RawData_Traits")
+
+# Raw leaf area data
+get_file(node = "smbqh",
+         file = "PFTC4_Scalbard_Raw_LeafArea_2018.csv",
+         path = "traits/data",
+         remote_path = "RawData/RawData_Traits")
+
+# NEED TO ADD CHEMICAL LEAF TRAITS
+
+
+#### COMMUNITY ####
+# Raw community data
+get_file(node = "smbqh",
+         file = "PFTC4_Svalbard_2018_Community.csv",
+         path = "community/data",
+         remote_path = "RawData/RawData_Community")
+
+# Draba dictionary
+get_file(node = "smbqh",
+         file = "PFTC4_Svalbard_2018_Draba_dictionary.xlsx",
+         path = "community/data",
+         remote_path = "RawData/RawData_Community")
