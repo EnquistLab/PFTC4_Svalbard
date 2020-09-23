8e286703e1c7fffe0e750930742d5b3b88dc1ea5
audhalbritter <me@audhalbritter.com>
2020-09-23 09:26:24 +0200

    update function

--- DataPaper/RunDataPaperDrakePlan.R
+++ DataPaper/RunDataPaperDrakePlan.R
@@ -6,7 +6,7 @@ library("drake")
 
 r_make(source = "DataPaper/R/DataPaperDrakePlan.R")
 loadd()
-failed()
+drake_failed()
 
 #view dependency graph
 r_vis_drake_graph(source = "DataPaper/R/DataPaperDrakePlan.R", targets_only = TRUE)
